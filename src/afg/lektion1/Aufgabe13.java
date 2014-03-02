d6050abc2060c021f20aaf9e37ff5c5a5e6b05aa
Mathias Weigert <har_lea_quin@gmx.de>
2014-03-02 12:28:19 +0100

    Etwas weiter an Afg. 1.3 gearbeitet

--- src/afg/lektion1/Aufgabe13.java
+++ src/afg/lektion1/Aufgabe13.java
@@ -33,28 +33,62 @@ public class Aufgabe13 {
 		output += "Integer: " + intWrapper + "\n";
 		output += "Long: " + longWrapper + "\n";
 		output += "Short: " + shortWrapper + "\n\n";
-		
+
 		output += "Die Werte werden bei der Initialisierung gesetzt.\n\n";
-		
+
 		byteWrapper = 23;
 		doubleWrapper = 666.666;
 		floatWrapper = 123.456f;
 		intWrapper = 12345;
 		longWrapper = 987654321l;
 		shortWrapper = -12345;
-		
+
 		output += "Byte: " + byteWrapper + "\n";
 		output += "Double: " + doubleWrapper + "\n";
 		output += "Float: " + floatWrapper + "\n";
 		output += "Integer: " + intWrapper + "\n";
 		output += "Long: " + longWrapper + "\n";
 		output += "Short: " + shortWrapper + "\n\n";
-		
-		output += "Byte:\n\n";
-		
-		output += "byteValue(): " + byteWrapper.byteValue() +"\n";
-		output += "hashCode(): " + byteWrapper.hashCode() +"\n";
-		output += "MAX_VALUE: " + byteWrapper.MAX_VALUE +"\n";
+
+		output += "Byte:\n";
+		output += "byteValue(): " + byteWrapper.byteValue() + "\n";
+		output += "hashCode(): " + byteWrapper.hashCode() + "\n";
+		output += "MAX_VALUE: " + byteWrapper.MAX_VALUE + "\n";
+		output += "MIN_VALUE: " + byteWrapper.MIN_VALUE + "\n";
+		output += "SIZE: " + byteWrapper.SIZE + "\n";
+		output += "TYPE: " + byteWrapper.TYPE + "\n\n";
+
+		output += "Double:\n";
+		output += "doubleValue(): " + doubleWrapper.doubleValue() + "\n";
+		output += "hashCode(): " + doubleWrapper.hashCode() + "\n";
+		output += "MAX_EXPONENT: " + doubleWrapper.MAX_EXPONENT + "\n";
+		output += "MAX_VALUE: " + doubleWrapper.MAX_VALUE + "\n";
+		output += "MIN_EXPONENT: " + doubleWrapper.MIN_EXPONENT + "\n";
+		output += "MIN_NORMAL: " + doubleWrapper.MIN_NORMAL + "\n";
+		output += "MIN_VALUE: " + doubleWrapper.MIN_VALUE + "\n";
+		output += "NaN: " + doubleWrapper.NaN + "\n";
+		output += "NEGATIVE_INFINITY: " + doubleWrapper.NEGATIVE_INFINITY
+				+ "\n";
+		output += "POSITIVE_INFINITY: " + doubleWrapper.POSITIVE_INFINITY
+				+ "\n";
+		output += "SIZE: " + doubleWrapper.SIZE + "\n";
+		output += "toHexString(): " + Double.toHexString(doubleWrapper) + "\n";
+		output += "isInfinite(): " + doubleWrapper.isInfinite() + "\n";
+		output += "TYPE: " + doubleWrapper.TYPE + "\n";
+		output += "doubleValue(): " + doubleWrapper.doubleValue() + "\n\n";
+
+		output += "Float:\n";
+		output += "floatValue(): " + floatWrapper.floatValue() + "\n";
+		output += "hashCode(): " + floatWrapper.hashCode() + "\n";
+		output += "MAX_EXPONENT: " + floatWrapper.MAX_EXPONENT + "\n";
+		output += "MAX_VALUE: " + floatWrapper.MAX_VALUE + "\n";
+		output += "MIN_EXPONENT: " + floatWrapper.MIN_EXPONENT + "\n";
+		output += "MIN_NORMAL: " + floatWrapper.MIN_NORMAL + "\n";
+		output += "MIN_VALUE: " + floatWrapper.MIN_VALUE + "\n";
+		output += "floatToBits: " + Float.floatToIntBits(floatWrapper) + "\n";
+		output += "toHexString: " + Float.toHexString(floatWrapper) + "\n";
+		output += "isNaN: " + Float.isNaN(floatWrapper) + "\n";
+		output += "TYPE: " + floatWrapper.TYPE + "\n";
 	}
 
 	public String getOutput() {
