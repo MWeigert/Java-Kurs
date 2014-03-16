210af8770877fb1fc3826be715616ce09701fef6
Mathias Weigert <har_lea_quin@gmx.de>
2014-03-16 09:38:08 +0100

    Neue Aufgaben hinzugefuegt

--- README.md
+++ README.md
@@ -36,3 +36,87 @@ StringBuilder und String.
 
 Führen Sie in einem Loop Konkatenierungs-Operationen aus für StringBuilder und
 String und vergleichen Sie die Performance der beiden Klassen.
+
+## Lektion 2
+
+###Aufgabe 2.1
+Schreiben Sie ein kleines Programm, in dem Sie die Java Operatoren <<, >>, >>>, ^, 
+& und  | auf ihre Funktionalität testen.
+
+###Aufgabe 2.2
+Schreiben Sie ein kleines Programm, in dem Sie eine Klasse Kunde mit einigen 
+Datenfeldern definieren. Überschreiben Sie die equals()-Methode für die Klasse 
+Kunde dahin, dass Objekte der Klasse Kunde auf Gleichheit der Datenfelder überprüft 
+werden.
+
+###Aufgabe 2.3
+Schreiben Sie ein kleines Programm, in dem Sie einen zweidimensionalen Array 
+definieren, erzeugen und initialisieren, initialisieren Sie den Array auf zwei 
+verschiedene Arten.
+
+###Aufgabe 2.4
+Schreiben Sie ein kleines Programm bestehend aus einer Klasse, definieren Sie in der 
+Klasse ein Datenfeld vom Typ ArrayList .
+
+Überprüfen Sie in Ihrer Anwendung die Funktionsweise der Methoden und die Features 
+der Klasse ArrayList.
+
+Wenden Sie dir arraycopy()-Methode der Klasse System auf die ArrayList an.
+
+Wenden Sie die Algorithmen zur Manipulation von Listen (siehe Folien) der Klasse 
+Collections auf die ArrayList an.
+
+##Lektion 3
+
+###Aufgabe 3.1
+Schreiben Sie ein kleines Programm, in dem Sie die unterschiedlichen loop-Konstrukte 
+auf ihre Funftionsfähigkeit hin überprüfen.
+
+###Aufgabe 3.2
+Schreiben Sie ein kleines Programm, in dem Sie drei Klassen mit beliebigen Datenfeldern 
+implementieren, 2 der Klassen sollen in einem Package abgelegt werden(eine davon soll 
+zusätzlich eine einfache innere Klasse haben), die andere Klasse in einem anderen Package, 
+sie soll eine Vererbungsbeziehung zu einer der beiden anderen Klassen haben, testen Sie 
+die Anwendung bezüglich unterschiedlicher Access Modifiers.
+
+###Aufgabe 3.3
+Implementieren Sie eine Klasse Outer mit einem Datenfeld x und einer Methode m1().
+
+Implementieren Sie nun in der Methode m1() eine lokale Klasse Inner, die auf das 
+Datenfeld x der Klasse Outer zugreift und ihren Wert am Bildschirm ausgibt.
+
+Implementieren Sie schliesslich noch eine main-Methode, wo Sie die Methode m1() 
+aufrufen.
+
+Versuchen Sie in einem 2. Schritt, der lokalen Klasse einen Zugriffsmodifikator zuzuweisen, 
+was passiert und warum?
+
+###Aufgabe 3.4
+Implementieren Sie bitte eine Klasse Outer mit einer Methode m1(int y), in der Methode 
+definieren Sie bitte zunächst eine weitere lokale Variable x und anschliessend eine lokale 
+Klasse Inner, in der Sie auf die Variablen y und x zugreifen.
+
+Testen Sie Ihren Code in einer main-Methode.
+
+Untersuchen Sie zusätzlich noch die Funktionsweise von anonymen inneren Klassen und 
+statischen inneren Klassen.
+
+##Lektion 4
+
+###Aufgabe 4.1
+Schreiben Sie ein kleines Programm, in dem Sie die verschiedenen Möglichkeiten des 
+Überschreiben und Überladen von Methoden sowie das Verdecken von Datenfeldern überprüfen. 
+Zeigen Sie in dem Programm auch, wie Polymorphismus angewendet wird.
+
+###Aufgabe 4.2
+Schreiben Sie ein kleines Programm, in dem Sie eine Klasse B implementieren, diese Klasse 
+soll von einer abstrakten Klasse A abgeleitet sein die wiederum ein Interface mit zwei 
+Methodendeklarationen sowie zwei Konstanten implementiert, das Interface wiederum soll 
+zwei weitere Interfaces ableiten, jedes dieser Interfaces definiert eine Methodendeklaration. 
+Instanzieren Sie ein Objekt der Klasse B, greifen Sie auf die Konstanten zu und rufen Sie 
+die Methoden auf.
+
+###Aufgabe 4.3
+Implementieren Sie drei verschiedene Exceptions, die Vererbungsbeziehungen zueinander haben. 
+Zeigen Sie in Ihrer Anwendung, dass eine überschriebene Methode nur gerade die Exceptions 
+werfen kann, die die Originalmethode wirft.
--- src/afg/lektion1/Aufgabe13.java
+++ src/afg/lektion1/Aufgabe13.java
@@ -53,42 +53,59 @@ public class Aufgabe13 {
 		output += "Byte:\n";
 		output += "byteValue(): " + byteWrapper.byteValue() + "\n";
 		output += "hashCode(): " + byteWrapper.hashCode() + "\n";
-		output += "MAX_VALUE: " + byteWrapper.MAX_VALUE + "\n";
-		output += "MIN_VALUE: " + byteWrapper.MIN_VALUE + "\n";
-		output += "SIZE: " + byteWrapper.SIZE + "\n";
-		output += "TYPE: " + byteWrapper.TYPE + "\n\n";
+		output += "MAX_VALUE: " + Byte.MAX_VALUE + "\n";
+		output += "MIN_VALUE: " + Byte.MIN_VALUE + "\n";
+		output += "SIZE: " + Byte.SIZE + "\n";
+		output += "TYPE: " + Byte.TYPE + "\n\n";
 
 		output += "Double:\n";
 		output += "doubleValue(): " + doubleWrapper.doubleValue() + "\n";
 		output += "hashCode(): " + doubleWrapper.hashCode() + "\n";
-		output += "MAX_EXPONENT: " + doubleWrapper.MAX_EXPONENT + "\n";
-		output += "MAX_VALUE: " + doubleWrapper.MAX_VALUE + "\n";
-		output += "MIN_EXPONENT: " + doubleWrapper.MIN_EXPONENT + "\n";
-		output += "MIN_NORMAL: " + doubleWrapper.MIN_NORMAL + "\n";
-		output += "MIN_VALUE: " + doubleWrapper.MIN_VALUE + "\n";
-		output += "NaN: " + doubleWrapper.NaN + "\n";
-		output += "NEGATIVE_INFINITY: " + doubleWrapper.NEGATIVE_INFINITY
+		output += "MAX_EXPONENT: " + Double.MAX_EXPONENT + "\n";
+		output += "MAX_VALUE: " + Double.MAX_VALUE + "\n";
+		output += "MIN_EXPONENT: " + Double.MIN_EXPONENT + "\n";
+		output += "MIN_NORMAL: " + Double.MIN_NORMAL + "\n";
+		output += "MIN_VALUE: " + Double.MIN_VALUE + "\n";
+		output += "NaN: " + Double.NaN + "\n";
+		output += "NEGATIVE_INFINITY: " + Double.NEGATIVE_INFINITY
 				+ "\n";
-		output += "POSITIVE_INFINITY: " + doubleWrapper.POSITIVE_INFINITY
+		output += "POSITIVE_INFINITY: " + Double.POSITIVE_INFINITY
 				+ "\n";
-		output += "SIZE: " + doubleWrapper.SIZE + "\n";
+		output += "SIZE: " + Double.SIZE + "\n";
 		output += "toHexString(): " + Double.toHexString(doubleWrapper) + "\n";
 		output += "isInfinite(): " + doubleWrapper.isInfinite() + "\n";
-		output += "TYPE: " + doubleWrapper.TYPE + "\n";
-		output += "doubleValue(): " + doubleWrapper.doubleValue() + "\n\n";
+		output += "TYPE: " + Double.TYPE + "\n\n";
 
 		output += "Float:\n";
 		output += "floatValue(): " + floatWrapper.floatValue() + "\n";
 		output += "hashCode(): " + floatWrapper.hashCode() + "\n";
-		output += "MAX_EXPONENT: " + floatWrapper.MAX_EXPONENT + "\n";
-		output += "MAX_VALUE: " + floatWrapper.MAX_VALUE + "\n";
-		output += "MIN_EXPONENT: " + floatWrapper.MIN_EXPONENT + "\n";
-		output += "MIN_NORMAL: " + floatWrapper.MIN_NORMAL + "\n";
-		output += "MIN_VALUE: " + floatWrapper.MIN_VALUE + "\n";
+		output += "MAX_EXPONENT: " + Float.MAX_EXPONENT + "\n";
+		output += "MAX_VALUE: " + Float.MAX_VALUE + "\n";
+		output += "MIN_EXPONENT: " + Float.MIN_EXPONENT + "\n";
+		output += "MIN_NORMAL: " + Float.MIN_NORMAL + "\n";
+		output += "MIN_VALUE: " + Float.MIN_VALUE + "\n";
 		output += "floatToBits: " + Float.floatToIntBits(floatWrapper) + "\n";
 		output += "toHexString: " + Float.toHexString(floatWrapper) + "\n";
 		output += "isNaN: " + Float.isNaN(floatWrapper) + "\n";
-		output += "TYPE: " + floatWrapper.TYPE + "\n";
+		output += "TYPE: " + Float.TYPE + "\n\n";
+		
+		output += "Long:\n";
+		output += "hashCode(): " + longWrapper.hashCode() + "\n";
+		output += "longValue(): " + longWrapper.longValue() + "\n";
+		output += "MAX_VALUE: " + Long.MAX_VALUE + "\n";
+		output += "MIN_VALUE: " + Long.MIN_VALUE + "\n";
+		output += "SIZE: " + Long.SIZE + "\n";
+		output += "bitCount(): " + Long.bitCount(longWrapper) + "\n";
+		output += "highestOneBit(): " + Long.highestOneBit(longWrapper) + "\n";
+		output += "lowestOneBit(): " + Long.lowestOneBit(longWrapper) + "\n";
+		output += "numberOfLeadingZeros(): " + Long.numberOfLeadingZeros(longWrapper) + "\n";
+		output += "numberOfTrailingZeros(): " + Long.numberOfTrailingZeros(longWrapper) + "\n";
+		output += "reverse(): " + Long.reverse(longWrapper) + "\n";
+		output += "signum(): " + Long.signum(longWrapper) + "\n";
+		output += "TYPE: " + Long.TYPE + "\n\n";
+		
+		output += "Short:\n";
+		output += "shortValue: " + shortWrapper.shortValue()+"\n";
 	}
 
 	public String getOutput() {
